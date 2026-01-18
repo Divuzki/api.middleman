b0f75d05b67c7fb8428445d3ca7a94e65daf9949
Divine Ikhuoria <divuzki@gmail.com>
2026-01-18 01:12:03 +0100

    test(wallet): add url routing tests for deposit views
    Add test cases to verify correct URL routing for DepositView and VerifyDepositView

--- wallet/tests.py
+++ wallet/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import resolve
+from .views import DepositView, VerifyDepositView
 
-# Create your tests here.
+class UrlRoutingTests(SimpleTestCase):
+    def test_verify_deposit_url_resolves_correctly(self):
+        # This URL should resolve to VerifyDepositView
+        url = '/users/deposit/verify/ref_cbb7075548e0/'
+        resolver = resolve(url)
+        self.assertEqual(resolver.func.view_class, VerifyDepositView)
+        self.assertEqual(resolver.kwargs['reference'], 'ref_cbb7075548e0')
+
+    def test_deposit_url_resolves_correctly(self):
+        url = '/users/deposit/'
+        resolver = resolve(url)
+        self.assertEqual(resolver.func.view_class, DepositView)
